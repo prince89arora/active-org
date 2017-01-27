80a7cb49484ceeb8d0d91aa77e0d0375b402613e
Prince Arora <aroraprince151@gmail.com>
2017-01-27 18:22:13 +0530

    Encryption util updated

--- active-org-security/src/main/java/org/active/security/crypto/EncryptionUtil.java
+++ active-org-security/src/main/java/org/active/security/crypto/EncryptionUtil.java
@@ -1,7 +1,17 @@
 package org.active.security.crypto;
 
-/**
- *
- */
 public class EncryptionUtil {
+
+    public static Encryption getInstance(Type type) throws EncryptionTypeNotFound {
+        switch (type) {
+            case AES:
+                return AESEncryption.instance();
+            default:
+                throw new EncryptionTypeNotFound();
+        }
+    }
+  
+    public static enum Type {
+        AES
+    }
 }
